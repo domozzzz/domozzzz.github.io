ff12d45a7c32a1d713b796a6e3a12e7ecdf43dda
domozzzz <alistairplace@d-i89-219-252.student.eduroam.uq.edu.au>
2024-07-02 14:48:44 +1000

    added sticky navbar and resized div margins

---
+++ script.js
@@ -0,0 +1,25 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav li");   
+
+window.onscroll = () => {
+
+    var current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop - 60) {
+            current = section.getAttribute("id"); 
+        }
+    });
+
+    navLi.forEach((li) => {
+        li.classList.remove("active");
+
+        console.log(li.classList)
+
+
+        if (li.classList.contains(current)) {
+            li.classList.add("active");
+        }
+    });
+};
